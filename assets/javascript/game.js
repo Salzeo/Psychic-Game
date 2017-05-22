f5b7680f7a94f328e9d5c28d2a59ae166abd32cb
Sally Bradfield <salzeo@gmail.com>
2017-05-21 20:13:58 -0600

    most recent version

---
+++ assets/javascript/game.js
@@ -0,0 +1,33 @@
+
+var letters = ['a','b','c','d','e','f','g','h','i','j','k','l',
+    'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+//setting Variables for tracking wins, loses, guesses to 0
+var wins=0;
+var losses=0;
+var guesses=0;
+
+	var computerChoice = letters[Math.floor(Math.random() * letters.length)];
+	
+	console.log(computerChoice)
+
+	//
+
+document.onkeypress = function(event) {
+    var userGuess = event.key;
+
+    if(userGuess === computerChoice){
+        wins++;
+    }else{
+        guesses--;
+    }
+
+    if(guesses === 0){
+        losses++
+    }
+
+    document.getElementById('wins').innerHTML = "Wins: " + wins;
+    document.getElementById('losses').innerHTML = "losses: " + losses;
+    document.getElementById('guesses').innerHTML = "Guesses left: " + guesses;
+
+}  // moved to below the innerHTML update
